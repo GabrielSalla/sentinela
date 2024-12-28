bcd28a24ca84ede7712fdacb406aded063939891
Gabriel Salla <gabriel.c.salla@gmail.com>
2024-12-28 18:29:57 -0300

    remove unused imports from slack plugin

--- src/plugins/slack/__init__.py
+++ src/plugins/slack/__init__.py
@@ -1,28 +1,8 @@
 from . import actions, services
 from .notifications.slack_notification import SlackNotification
-from .slack import (
-    MessageButton,
-    build_attachments,
-    delete,
-    get_actions_block,
-    get_context_block,
-    get_header_block,
-    get_section_block,
-    send,
-    update,
-)
 
 __all__ = [
     "actions",
-    "build_attachments",
-    "delete",
-    "get_actions_block",
-    "get_context_block",
-    "get_header_block",
-    "get_section_block",
-    "MessageButton",
-    "send",
-    "SlackNotification",
-    "update",
     "services",
+    "SlackNotification",
 ]
