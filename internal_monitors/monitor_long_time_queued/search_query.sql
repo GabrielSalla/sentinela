67a22ee17127f970fbf18cdbf241a3ead4fe5987
Gabriel Salla <gabriel.c.salla@gmail.com>
2025-01-05 11:24:09 -0300

    adapt internal monitor long time queued

--- internal_monitors/monitor_long_time_queued/search_query.sql
+++ internal_monitors/monitor_long_time_queued/search_query.sql
@@ -1,7 +1,7 @@
 with monitors as (
   select
     id as monitor_id,
-    enabled as monitor_enabled
+    enabled as monitor_enabled,
     (queued or running) as monitor_pending,
     extract(epoch from current_timestamp - greatest(queued_at, running_at)) :: int as seconds_queued
   from "Monitors"
--- internal_monitors/monitor_long_time_queued/update_query.sql
+++ internal_monitors/monitor_long_time_queued/update_query.sql
@@ -1,9 +1,9 @@
 with monitors as (
   select
     id as monitor_id,
-    enabled as monitor_enabled
+    enabled as monitor_enabled,
     (queued or running) as monitor_pending,
-    extract(epoch from current_timestamp - least(search_executed_at, update_executed_at)) :: int as seconds_queued
+    extract(epoch from current_timestamp - greatest(queued_at, running_at)) :: int as seconds_queued
   from "Monitors"
   where
     id = any($1 :: int[])
