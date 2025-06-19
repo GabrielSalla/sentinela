68169e3b88b46aff186aeca4d70d26aebea8cac0
Gabriel Salla <gabriel.c.salla@gmail.com>
2025-06-19 17:46:07 -0300

    fix monitors stuck procedure query

--- src/components/controller/procedures/sql_files/monitors_stuck.sql
+++ src/components/controller/procedures/sql_files/monitors_stuck.sql
@@ -4,7 +4,10 @@ with monitors as (
     extract(epoch from current_timestamp - last_heartbeat) :: int as time_since_last_heartbeat
   from "Monitors"
   where
-    enabled and (queued or running)
+    enabled and
+    (queued or running) and
+    coalesce(queued_at < current_timestamp -  ($1 :: int) * interval '1 second', true) and
+    coalesce(running_at < current_timestamp -  ($1 :: int) * interval '1 second', true)
 )
 select id
 from monitors
