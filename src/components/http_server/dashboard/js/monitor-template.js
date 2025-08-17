bf38d771d2ee4ad3ec86d2bde069e90d82bf6267
Gabriel Salla <gabriel.salla@pagar.me>
2025-08-17 17:11:22 -0300

    update monitor template

--- src/components/http_server/dashboard/js/monitor-template.js
+++ src/components/http_server/dashboard/js/monitor-template.js
@@ -3,23 +3,19 @@ const MONITOR_TEMPLATE = `from typing import TypedDict
 from monitor_utils import IssueOptions, MonitorOptions
 from notifications.internal_monitor_notification import internal_monitor_notification
 
-# Monitor configuration
 monitor_options = MonitorOptions(
     search_cron="* * * * *",
     update_cron="* * * * *",
 )
 
-# Issue configuration
 issue_options = IssueOptions(
     model_id_key="",
     solvable=True,
 )
 
-# Alert configuration
 alert_options = None
 
 
-# Define your issue data structure
 class IssueDataType(TypedDict):
     pass
 
