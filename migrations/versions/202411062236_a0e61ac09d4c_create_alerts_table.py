cacf63f53e732def84d38fe87c2a849951739683
Gabriel Salla <gabriel.salla@pagar.me>
2025-08-21 18:55:29 -0300

    migrations adjustments

--- migrations/versions/202411062236_a0e61ac09d4c_create_alerts_table.py
+++ migrations/versions/202411062236_a0e61ac09d4c_create_alerts_table.py
@@ -11,7 +11,6 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-# revision identifiers, used by Alembic.
 revision: str = "a0e61ac09d4c"
 down_revision: Union[str, None] = "61f4515a5943"
 branch_labels: Union[str, Sequence[str], None] = None
--- migrations/versions/202411062248_8382681faa0c_create_issues_table.py
+++ migrations/versions/202411062248_8382681faa0c_create_issues_table.py
@@ -11,7 +11,6 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-# revision identifiers, used by Alembic.
 revision: str = "8382681faa0c"
 down_revision: Union[str, None] = "a0e61ac09d4c"
 branch_labels: Union[str, Sequence[str], None] = None
