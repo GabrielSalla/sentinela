938e1d2d72cf580ab202df4966b361075dcd1f45
Gabriel Salla <gabriel.salla@pagar.me>
2025-08-23 10:38:28 -0300

    add check database function

--- src/internal_database/__init__.py
+++ src/internal_database/__init__.py
@@ -1,8 +1,10 @@
+from .check_database import check_database
 from .exceptions import PendingDatabaseUpgrade
 from .internal_database import CallbackSession, close, engine, get_session
 
 __all__ = [
     "CallbackSession",
+    "check_database",
     "close",
     "engine",
     "get_session",
