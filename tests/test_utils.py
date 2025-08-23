462830b1ed632f22cdb6996a67c60a4c46e634c4
Gabriel Salla <gabriel.salla@pagar.me>
2025-08-23 11:41:04 -0300

    test log utils checks exception text

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,10 +6,12 @@ def assert_message_in_log(caplog, pattern: str, count: int = 1, regex: bool = Fa
     found = 0
     for record in caplog.records:
         if regex:
-            if re.search(pattern, record.message):
+            message_match = re.search(pattern, record.message)
+            exception_match = record.exc_text and re.search(pattern, record.exc_text)
+            if message_match or exception_match:
                 found += 1
         else:
-            if pattern in record.message:
+            if pattern in record.message or (record.exc_text and pattern in record.exc_text):
                 found += 1
     if found != count:
         assert False, f"Expected {count} log messages '{pattern}', found {found}"
@@ -19,8 +21,10 @@ def assert_message_not_in_log(caplog, pattern: str, regex: bool = False):
     """Check if the logs do not contain the specified message"""
     for record in caplog.records:
         if regex:
-            if re.search(pattern, record.message):
+            message_match = re.search(pattern, record.message)
+            exception_match = record.exc_text and re.search(pattern, record.exc_text)
+            if message_match or exception_match:
                 assert False, f"Found log message: {pattern}"
         else:
-            if pattern in record.message:
+            if pattern in record.message or (record.exc_text and pattern in record.exc_text):
                 assert False, f"Found log message: {pattern}"
