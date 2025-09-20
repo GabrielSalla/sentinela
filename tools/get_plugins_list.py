e6ff4affd7d3d092779b8f69438b472073fc899f
Gabriel Salla <gabriel.c.salla@gmail.com>
2025-09-20 19:15:36 -0300

    allow environment for get plugins list tool

--- tools/get_plugins_list.py
+++ tools/get_plugins_list.py
@@ -2,12 +2,23 @@
 dependencies"""
 
 import os
+import sys
 
 import yaml
+
+
+def _make_plugin_name(environment: str | None, plugin: str) -> str:
+    plugin_name = f"plugin-{plugin.strip()}"
+    if environment:
+        return f"{plugin_name}-{environment}"
+    return plugin_name
+
+
+environment = sys.argv[1] if len(sys.argv) > 1 else None
 
 with open(os.environ.get("CONFIGS_FILE", "configs.yaml"), "r") as file:
     configs = yaml.load(file.read(), Loader=yaml.FullLoader)
 
 if configs["plugins"]:
-    plugins_list = [f"plugin-{plugin.strip()}" for plugin in configs["plugins"]]
+    plugins_list = [_make_plugin_name(environment, plugin) for plugin in configs["plugins"]]
     print(",".join(plugins_list))
