62b7bf36208430168e06cc05b4db6705a23577db
Gabriel Salla <gabriel.c.salla@gmail.com>
2026-06-04 17:44:40 -0300

    validate issue route params

--- src/components/http_server/issue_routes.py
+++ src/components/http_server/issue_routes.py
@@ -1,6 +1,7 @@
 from aiohttp import web
 from aiohttp.web_request import Request
 from aiohttp.web_response import Response
+from pydantic import BaseModel
 
 import commands as commands
 
@@ -8,11 +9,16 @@ issue_routes = web.RouteTableDef()
 base_route = "/issue"
 
 
+class _IssueIdPathParams(BaseModel):
+    issue_id: int
+
+
 @issue_routes.post(base_route + "/{issue_id}/drop")
 @issue_routes.post(base_route + "/{issue_id}/drop/")
 async def issue_drop(request: Request) -> Response:
     """Route to drop an issue"""
-    issue_id = int(request.match_info["issue_id"])
+    params = _IssueIdPathParams.model_validate({"issue_id": request.match_info["issue_id"]})
+    issue_id = params.issue_id
     try:
         await commands.issue_drop(issue_id)
     except commands.IssueNotFoundError as e:
