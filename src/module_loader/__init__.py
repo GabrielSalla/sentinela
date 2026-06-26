257650dcecbccf416c049302617072ef10de1a9e
Gabriel Salla <gabriel.c.salla@gmail.com>
2026-06-26 15:06:44 -0300

    create import restriction functions

--- src/module_loader/__init__.py
+++ src/module_loader/__init__.py
@@ -1,4 +1,5 @@
 from .checker import check_module
+from .import_restrict import scan_imports, scan_nested_imports
 from .loader import (
     create_module_files,
     load_module_from_file,
@@ -14,4 +15,6 @@ __all__ = [
     "load_module_from_string",
     "make_module_name",
     "remove_module",
+    "scan_imports",
+    "scan_nested_imports",
 ]
