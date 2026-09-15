7434666e4e6e7adc50988fdc370af79470cac6a2
Gabriel Salla <gabriel.c.salla@gmail.com>
2026-09-15 10:52:25 -0300

    create function to check if command is allowed

--- src/components/http_server/command_config.py
+++ src/components/http_server/command_config.py
@@ -2,6 +2,7 @@ from aiohttp import web
 from aiohttp.web_response import Response
 
 from configs import configs
+from models import UserRole
 
 
 def is_command_enabled(command: str) -> bool:
@@ -22,3 +23,18 @@ def disabled_command_response(command: str) -> Response:
         },
         status=403,
     )
+
+
+def is_command_allowed(command: str, role: UserRole) -> bool:
+    """Return whether a role can execute a command. Admin can execute any command"""
+    if role == UserRole.admin:
+        return True
+    command_config = configs.http_server.commands.get(command, None)
+    if command_config is None:
+        return True
+    return command_config.required_role == role.name
+
+
+def forbidden_command_response() -> Response:
+    """Return a response indicating that the user role cannot execute a command"""
+    return web.json_response({"status": "forbidden"}, status=403)
